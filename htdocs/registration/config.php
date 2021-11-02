e4c16dcd29edbffbe1df3194892e22f610bbc954
veriperi <84374417+veriperi@users.noreply.github.com>
2021-11-02 23:04:18 +0530

    Add files via upload

---
+++ htdocs/registration/config.php
@@ -0,0 +1,13 @@
+<?php
+
+$hostname = "localhost";
+$username = "root";
+$password = "123456";
+$database = "login_register";
+
+$conn = mysqli_connect($hostname, $username, $password, $database) or die("Database connection failed");
+
+$base_url = "https://code.akhfasoft.net/login-register-youtube/";
+$my_email = "code@akhfasoft.net";
+
+?>
